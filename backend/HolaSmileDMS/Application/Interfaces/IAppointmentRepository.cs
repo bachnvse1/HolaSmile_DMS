59069c988942c292360feee44b01828a64b39876
Xuan Phuc <141129680+phucz2103@users.noreply.github.com>
2025-06-18 22:07:01 +0700

    Create command,hancler,dto

--- backend/HolaSmileDMS/Application/Interfaces/IAppointmentRepository.cs
+++ backend/HolaSmileDMS/Application/Interfaces/IAppointmentRepository.cs
@@ -5,5 +5,9 @@ namespace HDMS_API.Application.Interfaces
     public interface IAppointmentRepository
     {
         Task<Appointment> CreateAppointmentAsync(BookAppointmentCommand request, int patientId);
+        Task<Appointment?> GetAllAppointmentAsync(int appointmentId);
+        Task<Appointment> GetAppointmentByIdsAsync();
+        Task<List<Appointment>> GetAppointmentsByPatientIdAsync(int patientId);
+
     }
 }
---
+++ backend/HolaSmileDMS/Application/Usecases/UserCommon/Appointment/ViewApponintmentHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Application.Usecases.UserCommon.Appointment
+{
+    public class ViewApponintmentHandler : IRequestHandler<ViewAppointmentCommand, List<AppointmentDTO>>
+    {
+
+        public Task<List<AppointmentDTO>> Handle(ViewAppointmentCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
