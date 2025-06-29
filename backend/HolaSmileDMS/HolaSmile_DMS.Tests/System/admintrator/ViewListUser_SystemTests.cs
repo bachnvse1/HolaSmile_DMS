9d69a1bbb2efed4ce1fb4232f175c2ceae8cbbf0
Xuan Phuc <141129680+phucz2103@users.noreply.github.com>
2025-06-29 22:17:59 +0700

    fix test

--- backend/HolaSmileDMS/HolaSmile_DMS.Tests/System/admintrator/ViewListUser_SystemTests.cs
+++ backend/HolaSmileDMS/HolaSmile_DMS.Tests/System/admintrator/ViewListUser_SystemTests.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System.Net;
+//using System.Net.Http.Json;
+//using Application.Usecases.Admintrator;
+//using Org.BouncyCastle.Asn1.X509;
+//using Xunit;
 
-namespace HolaSmile_DMS.Tests.System.admintrator
-{
-    internal class ViewListUser_SystemTests
-    {
-    }
-}
+//public class ViewListUser_SystemTests : IClassFixture<CustomWebAppFactory<Program>>
+//{
+//    private readonly HttpClient _client;
+
+//    public ViewListUser_SystemTests(CustomWebAppFactory<Program> factory)
+//    {
+//        _client = factory.CreateClient();
+//    }
+
+//    [Fact(DisplayName = "[System-Normal] Admin_Can_Get_User_List")]
+//    public async Task Admin_Can_Get_User_List()
+//    {
+//        var token = await Helper.GenerateJwtTokenAsync("admintrator", 1);
+//        _client.DefaultRequestHeaders.Authorization = new("Bearer", token);
+
+//        var response = await _client.GetAsync("/api/users");
+
+//        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+//        var users = await response.Content.ReadFromJsonAsync<List<ViewListUserDTO>>();
+//        Assert.NotNull(users);
+//    }
+
+//    [Fact(DisplayName = "[System-Abnormal] Unauthorized_User_Throws_Forbidden")]
+//    public async Task Unauthorized_User_Throws_Forbidden()
+//    {
+//        var token = await Helper.GenerateJwtTokenAsync("receptionist", 5);
+//        _client.DefaultRequestHeaders.Authorization = new("Bearer", token);
+
+//        var response = await _client.GetAsync("/api/users");
+
+//        Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+//    }
+//}
