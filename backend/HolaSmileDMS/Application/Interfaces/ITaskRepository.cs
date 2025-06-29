9f73c8aea8aed646695a9786f0dcebed1203c5be
Ducnm21 <ducnguyenm.to@gmail.com>
2025-06-29 23:42:10 +0700

    View Task Details

--- backend/HolaSmileDMS/Application/Interfaces/ITaskRepository.cs
+++ backend/HolaSmileDMS/Application/Interfaces/ITaskRepository.cs
@@ -3,5 +3,6 @@
     public interface ITaskRepository
     {
         Task<bool> CreateTaskAsync(Task task, CancellationToken cancellationToken);
+        Task<Task?> GetTaskByIdAsync(int taskId, CancellationToken cancellationToken);
     }
 }
