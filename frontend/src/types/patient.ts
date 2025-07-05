9892b943664c12a0442b53a4ff8cab741ea88e89
dungpthe172459 <dungpthe172459@fpt.edu.vn>
2025-07-05 11:11:54 +0700

    Update UI

--- frontend/src/types/patient.ts
+++ frontend/src/types/patient.ts
@@ -1,9 +1,22 @@
 export interface Patient {
-    userId: number
-    patientId: number
-    fullname: string
-    gender: string
-    phone: string
-    dob: string
-    email: string
+  userId: number
+  patientId: number
+  fullname: string
+  gender: string
+  phone: string
+  dob: string
+  email: string
+  address?: string
+  underlyingConditions?: string
 }
+
+export interface EditPatientForm {
+  fullName: string
+  dob: string
+  gender: boolean
+  email: string
+  address: string
+  underlyingConditions?: string
+}
+
+
