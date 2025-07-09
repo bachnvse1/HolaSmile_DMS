a09a525cd333c9aece44326711408b3970ec3a1c
dungpthe172459 <dungpthe172459@fpt.edu.vn>
2025-07-09 09:43:42 +0700

    tạo UI và tích hợp API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,6 +33,7 @@ import { EditSupplyPage } from './pages/supply/EditSupplyPage';
 import { SupplyDetailPage } from './pages/supply/SupplyDetailPage';
 import UserManagement from './pages/auth/UserManagement';
 import AssignedTasks from './pages/assistant/AssignedTasks';
+import ProcedureManagement from './pages/proceduce/ProcedureManagement';
 function App() {
   return (
     <>
@@ -73,6 +74,7 @@ function App() {
         <Route path="/inventory/:supplyId/edit" element={<EditSupplyPage />} />
         <Route path="/administrator/user-list" element={<UserManagement/>}/>
         <Route path="/assistant/assigned-tasks" element={<AssignedTasks/>}/>
+        <Route path="/proceduces" element={<ProcedureManagement/>}/>
       </Routes>
     </>
   );
