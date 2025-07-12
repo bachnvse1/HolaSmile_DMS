e11c7230e346659ac8a6b972d103a277ed518169
dungpthe172459 <dungpthe172459@fpt.edu.vn>
2025-07-12 18:26:01 +0700

    Update UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,6 +29,7 @@ import OrthodonticTreatmentPlansPage from './pages/OrthodonticTreatmentPlans';
 import OrthodonticTreatmentPlanFormPage from './pages/OrthodonticTreatmentPlanFormPage';
 import FUAppointmentPage from './pages/appointment/FUAppointmentPage';
 import UserManagement from './pages/auth/UserManagement';
+import InvoiceList from './pages/invoice/InvoiceList';
 function App() {
   return (
     <>
@@ -64,6 +65,7 @@ function App() {
         <Route path="/patients/:patientId/orthodontic-treatment-plans/:planId/edit" element={<OrthodonticTreatmentPlanFormPage />} />
         <Route path="/patient/follow-up" element={<FUAppointmentPage />} />
         <Route path="/administrator/user-list" element={<UserManagement/>}/>
+        <Route path="/invoices" element={<InvoiceList/>}/>
       </Routes>
     </>
   );
