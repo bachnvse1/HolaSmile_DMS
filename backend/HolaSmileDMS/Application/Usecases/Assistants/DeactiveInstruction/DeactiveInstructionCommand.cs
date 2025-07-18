449096336055ef88537886ae16518b65ab43e42a
Ducnm21 <ducnguyenm.to@gmail.com>
2025-07-18 18:50:17 +0700

    Deactive Instruction + Test

---
+++ backend/HolaSmileDMS/Application/Usecases/Assistants/DeactiveInstruction/DeactiveInstructionCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Application.Usecases.Assistants.DeactiveInstruction
+{
+    public class DeactiveInstructionCommand : IRequest<string>
+    {
+        public int InstructionId { get; }
+
+        public DeactiveInstructionCommand(int instructionId)
+        {
+            InstructionId = instructionId;
+        }
+    }
+}
