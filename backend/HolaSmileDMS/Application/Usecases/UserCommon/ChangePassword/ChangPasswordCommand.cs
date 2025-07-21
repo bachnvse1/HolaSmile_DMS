86e10b9275fa7932ca9f1f24a140da8c11c19ec2
Ducnm21 <ducnguyenm.to@gmail.com>
2025-07-21 13:59:15 +0700

    Change Password + Test

---
+++ backend/HolaSmileDMS/Application/Usecases/UserCommon/ChangePassword/ChangPasswordCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Usecases.UserCommon.ChangePassword;
+
+public class ChangePasswordCommand : IRequest<string>
+{
+    public string CurrentPassword { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
