bdcf5d0788b31711707569f93ecd1be6fc9e1471
Xuan Phuc <141129680+phucz2103@users.noreply.github.com>
2025-08-06 16:31:23 +0700

    update chatbot and test

--- backend/HolaSmileDMS/Application/Usecases/Administrators/UpdateChatbotData/UpdateChatbotDataHandler.cs
+++ backend/HolaSmileDMS/Application/Usecases/Administrators/UpdateChatbotData/UpdateChatbotDataHandler.cs
@@ -22,7 +22,7 @@ namespace Application.Usecases.Administrators.UpdateChatbotData
             var currentUserRole = user?.FindFirst(ClaimTypes.Role)?.Value;
             var currentUserId = int.Parse(user?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "0");
 
-            if (!string.Equals(currentUserRole, "patient", StringComparison.OrdinalIgnoreCase))
+            if (!string.Equals(currentUserRole, "owner", StringComparison.OrdinalIgnoreCase) && !string.Equals(currentUserRole, "Administrator", StringComparison.OrdinalIgnoreCase))
             {
                 throw new UnauthorizedAccessException(MessageConstants.MSG.MSG26);
             }
