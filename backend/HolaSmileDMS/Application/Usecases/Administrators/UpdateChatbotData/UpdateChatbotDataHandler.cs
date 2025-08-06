43f19c1e65300c8aeeedc625e3354fb7dd5c91df
Xuan Phuc <141129680+phucz2103@users.noreply.github.com>
2025-08-06 10:43:32 +0700

    add check author

--- backend/HolaSmileDMS/Application/Usecases/Administrators/UpdateChatbotData/UpdateChatbotDataHandler.cs
+++ backend/HolaSmileDMS/Application/Usecases/Administrators/UpdateChatbotData/UpdateChatbotDataHandler.cs
@@ -1,5 +1,8 @@
-﻿using Application.Interfaces;
+﻿using System.Security.Claims;
+using Application.Constants;
+using Application.Interfaces;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Application.Usecases.Administrators.UpdateChatbotData
@@ -7,12 +10,22 @@ namespace Application.Usecases.Administrators.UpdateChatbotData
     public class UpdateChatbotDataHandler : IRequestHandler<UpdateChatbotDataCommand, bool>
     {
         private readonly IChatBotKnowledgeRepository _chatbotRepo;
-        public UpdateChatbotDataHandler(IChatBotKnowledgeRepository chatbotRepo)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public UpdateChatbotDataHandler(IChatBotKnowledgeRepository chatbotRepo, IHttpContextAccessor httpContextAccessor)
         {
             _chatbotRepo = chatbotRepo;
+            _httpContextAccessor = httpContextAccessor;
         }
         public async Task<bool> Handle(UpdateChatbotDataCommand request, CancellationToken cancellationToken)
         {
+            var user = _httpContextAccessor.HttpContext?.User;
+            var currentUserRole = user?.FindFirst(ClaimTypes.Role)?.Value;
+            var currentUserId = int.Parse(user?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "0");
+
+            if (!string.Equals(currentUserRole, "patient", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new UnauthorizedAccessException(MessageConstants.MSG.MSG26);
+            }
             var existingKnowledge = await _chatbotRepo.GetByIdAsync(request.KnowledgeId);
             if (request.NewAnswer.Trim().IsNullOrEmpty())
             {
