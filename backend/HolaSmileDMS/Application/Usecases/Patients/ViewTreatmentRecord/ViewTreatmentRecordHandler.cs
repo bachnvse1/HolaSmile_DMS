72f8ba247726fb731e45f21aca6f880efdad0c94
bachnvse <Bachnvhe172297@fpt.edu.vn>
2025-08-09 11:11:21 +0700

    fix raimingAmount

--- backend/HolaSmileDMS/Application/Usecases/Patients/ViewTreatmentRecord/ViewTreatmentRecordHandler.cs
+++ backend/HolaSmileDMS/Application/Usecases/Patients/ViewTreatmentRecord/ViewTreatmentRecordHandler.cs
@@ -69,6 +69,9 @@ public sealed class ViewPatientTreatmentRecordHandler
                 if (latestInvoice != null)
                 {
                     record.RemainingAmount = latestInvoice.RemainingAmount;
+                } else
+                {
+                    record.RemainingAmount = record.TotalAmount;
                 }
             }
         }
