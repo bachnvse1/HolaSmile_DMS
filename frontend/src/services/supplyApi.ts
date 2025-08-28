e82baf82b8d0dfb85a01742694696ea139bd6e2a
Cacao03 <namnhhe176500@fpt.edu.vn>
2025-08-28 18:25:00 +0700

    HDMSF-175 small fix

--- frontend/src/services/supplyApi.ts
+++ frontend/src/services/supplyApi.ts
@@ -12,11 +12,8 @@ export const mapToSupplyItem = (supply: Supply): SupplyItem => ({
   name: supply.Name,
   unit: supply.Unit,
   price: supply.Price,
-<<<<<<< HEAD
-=======
   inStock: supply.QuantityInStock,
   createdAt: supply.CreatedAt || null,
->>>>>>> 0fe3576cf4d39bc37fc08bccad7b9e368172de62
 })
 
 
